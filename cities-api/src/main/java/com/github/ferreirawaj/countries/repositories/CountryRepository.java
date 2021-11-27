f7676c7bead21b5caae987151e41d7ac0aaecd67
William Ferreira dos Anjos <89553302+ferreirawaj@users.noreply.github.com>
2021-11-27 00:09:44 -0300

    Delete cities-api directory

--- cities-api/src/main/java/com/github/ferreirawaj/countries/repositories/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.ferreirawaj.countries.repositories;
-
-import com.github.ferreirawaj.countries.entities.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
--- cities-api/src/main/java/com/github/ferreirawaj/staties/repositories/StateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.ferreirawaj.staties.repositories;
-
-import com.github.ferreirawaj.staties.entities.State;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StateRepository extends JpaRepository<State, Long> {
-}
